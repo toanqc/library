3db0e39267da95d4fc941f323fff90936ed19700
Bao Pham <ngocbaopt@gmail.com>
2015-06-30 18:17:35 -0500

    Check in the UI for checkout functionality

---
+++ src/mpp/library/controller/CheckoutController.java
@@ -0,0 +1,27 @@
+package mpp.library.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class CheckoutController {
+
+	@FXML protected void gotoMainScreen(MouseEvent event) {
+		System.out.println("Goto MainScreen");
+	}
+	
+	@FXML protected void proceedCheckout(MouseEvent event) {
+		System.out.println("proceed checkout");
+	}
+	
+	@FXML protected void handleCancel(MouseEvent event) {
+		System.out.println("handle Cancel");
+	}
+	
+	@FXML protected void printCheckoutRecord(MouseEvent event) {
+		System.out.println("Print Check out Record");
+	}
+	
+	@FXML protected void search(MouseEvent event) {
+		System.out.println("Search Member ID");
+	}
+}
