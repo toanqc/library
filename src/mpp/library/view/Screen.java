25938fb022605a4ebafd7187e3f5be3ddf15e444
Toan Quach <toanqc@gmail.com>
2015-07-02 20:26:42 -0500

    fix member list layout issue

--- src/mpp/library/view/Screen.java
+++ src/mpp/library/view/Screen.java
@@ -2,7 +2,7 @@ package mpp.library.view;
 
 public enum Screen {
 	HOME("home.fxml", 800, 630),
-	MEMBER_LIST("member/memberList.fxml", 800, 670),
+	MEMBER_LIST("member/memberList.fxml", 800, 620),
 	MEMBER("member/member.fxml", 530, 400),
 	PUBLICATION("",  0, 0),
 	CHECKOUT("checkout/Checkout.fxml", 520, 420),
